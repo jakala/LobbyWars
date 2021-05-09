146e774580f3e2ae5027cd94becfccccc822343a
jakala <angel.cid@gmail.com>
2021-05-09 16:21:01 +0200

    added exception subscriber to return json array in case of exception

---
+++ src/Infrastructure/EventListener/ExceptionSubscriber.php
@@ -0,0 +1,36 @@
+<?php
+namespace App\Infrastructure\EventListener;
+
+use Exception;
+use JetBrains\PhpStorm\ArrayShape;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class ExceptionSubscriber implements EventSubscriberInterface
+{
+    public function onKernelException(ExceptionEvent $event)
+    {
+        $e = $event->getThrowable();
+        if(!$e instanceOf \Exception) {
+            return ;
+        }
+
+        $data = [
+            'status' => false,
+            'message' => $e->getMessage()
+        ];
+
+        $response = new JsonResponse($data,$e->getStatusCode());
+        $response->headers->set('Content-Type', 'application/json');
+        $event->setResponse($response);
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::EXCEPTION => 'onKernelException',
+        ];
+    }
+}
